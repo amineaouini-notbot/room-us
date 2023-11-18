7946cc1230ca9e62e3f0e9739d2c176489c130bf
amineaouini-notbot <aminaouini565@gmail.com>
2023-11-18 22:42:05 +0100

    connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1/room-us')
+.then(()=>console.log('DB connected'))
 
 // create application/json parser
 app.use(bodyParser.urlencoded({ extended: false }))
