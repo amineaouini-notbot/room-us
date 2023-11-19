c957142911e227fa377ed52fceb7cd0dae578a2d
Geekyume <aminaouini565@gmail.com>
2023-11-19 22:40:16 +0100

    setup socket server front to back

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const http = require('http')
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
 const mongoose = require('mongoose')
-
+const {Server} = require('socket.io')
 mongoose.connect('mongodb://127.0.0.1/room-us')
 .then(()=>console.log('DB connected'))
 .catch(err => {throw err})
@@ -13,7 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+
+
+const server = http.Server(app)
+const io = new Server(server)
+
+require('./sockets/connection.js')(io)
+
 const port = 5000;
-app.listen(port, () => { // run server
+server.listen(port, () => { // run server
   console.log(`Server is running on port ${port}`);
 });
