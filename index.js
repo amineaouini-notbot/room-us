56e0b788b88647f074c00906d5927611f8700812
Geekyume <aminaouini565@gmail.com>
2023-11-19 15:21:55 +0100

    fixed style

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1/room-us')
 .then(()=>console.log('DB connected'))
+.catch(err => {throw err})
 
 // create application/json parser
 app.use(bodyParser.urlencoded({ extended: false }))
