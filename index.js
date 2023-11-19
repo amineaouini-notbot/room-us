38fb34a130f5d86b9ce1ef17cfb84878841186eb
Geekyume <aminaouini565@gmail.com>
2023-11-19 22:52:07 +0100

    setup socket for dev

--- index.js
+++ index.js
@@ -15,13 +15,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+const port = 5000;
 
 const server = http.Server(app)
-const io = new Server(server)
+const io = process.env.NODE_ENV === "production" ?
+  new Server(server) :
+  new Server({
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  })
 
-require('./sockets/connection.js')(io)
+require('./sockets/connection.js')(io, port)
 
-const port = 5000;
 server.listen(port, () => { // run server
   console.log(`Server is running on port ${port}`);
 });
