1f8d3010a594ca588d03ff87b6546876b8cb7230
Geekyume <aminaouini565@gmail.com>
2023-11-21 21:26:53 +0100

    empty input once msg is sent

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -5,19 +5,26 @@ import user from '../user';
 
 const Input = ()=>{
     const [input, changeInput] = useState("")
+
     const send = () => {
         if (input.length > 0) {         
-            let username = user.getUsername()
-            let message = input
-            
-            socket.emit('send-message', {username, message})
+            let newMsg = {
+
+                username: user.getUsername(),
+                message: input,
+                created_at: new Date()
+            }
+            socket.emit('send-message', newMsg)
+            changeInput('')
+            document.getElementById('msg-input').value = ''
+
         }
     }
 
     return(
         <div id='input'>
             
-            <input type='text' onChange={e => changeInput(e.target.value)}></input>
+            <input id='msg-input' type='text' onChange={e => changeInput(e.target.value)}></input>
             <button id='send' onClick={send}>Send</button>
         </div>
     )
