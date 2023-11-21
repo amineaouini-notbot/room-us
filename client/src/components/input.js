dfa0b71ec8fd5c6f0fd5be25df7019a0d3c28e56
Geekyume <aminaouini565@gmail.com>
2023-11-21 21:57:12 +0100

    send msg on enter key pressed

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -1,34 +1,65 @@
-import { useState } from 'react'
+import React from 'react'
 import { socket } from '../socket';
 import user from '../user';
 
 
-const Input = ()=>{
-    const [input, changeInput] = useState("")
-
-    const send = () => {
-        if (input.length > 0) {         
+class Input extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            input: ''
+        }
+        this.send = this.send.bind(this)
+        this.changeInput = this.changeInput.bind(this)
+        
+    }
+    
+    changeInput(e){
+        e.preventDefault()
+        this.setState({ input: e.target.value})
+        
+    }
+    
+    send(){
+        if (this.state.input.length > 0) {         
             let newMsg = {
 
                 username: user.getUsername(),
-                message: input,
+                message: this.state.input,
                 created_at: new Date()
             }
             socket.emit('send-message', newMsg)
-            changeInput('')
+            this.setState({input: ''})
             document.getElementById('msg-input').value = ''
 
         }
     }
 
-    return(
-        <div id='input'>
+    componentDidMount(){
+        var msgInput = document.getElementById("msg-input");
+
+        // Execute a function when the user presses a key on the keyboard
+        msgInput.addEventListener("keypress", function(event) {
+          // If the user presses the "Enter" key on the keyboard
+          if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            document.getElementById("send").click();
+          }
+        }); 
+    }
+
+    render(){
+        return(
+            <div id='input'>
             
-            <input id='msg-input' type='text' onChange={e => changeInput(e.target.value)}></input>
-            <button id='send' onClick={send}>Send</button>
-        </div>
-    )
-}
+            <input id='msg-input' type='text' onChange={this.changeInput}></input>
+            <button id='send' onClick={this.send}>Send</button>
+        </div>)
+
+    }
+} 
 
 
 export default Input;
