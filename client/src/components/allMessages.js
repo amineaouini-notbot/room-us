308dc363e9b15368bd8a7f624892c31b75368653
Geekyume <aminaouini565@gmail.com>
2023-11-22 11:49:25 +0100

    setup dynamic scroll button when scrolling to top

--- client/src/components/allMessages.js
+++ client/src/components/allMessages.js
@@ -3,14 +3,26 @@ import {Component} from 'react'
 import funcs from './funcs'
 import user from '../user'
 import { socket } from '../socket'
+import { GrLinkBottom } from "react-icons/gr";
 
 class AllMessages extends Component{
     constructor(props){
         super(props)
         this.state = {
-            messages: []
+            messages: [],
+            onBottom: true
         }
-        
+        this.onScroll = this.onScroll.bind(this)
+    }
+    onScroll(){
+        const element = document.getElementById("all-messages");
+        if(element.scrollTop < element.scrollHeight && this.state.onBottom){
+            console.log(element.scrollTop, element.scrollHeight)
+            this.setState({onBottom: false})
+        }
+        else if (element.scrollTop * 2 == element.scrollHeight){
+            this.setState({onBottom: true})
+        }
     }
     componentWillMount(){
         fetch('/api/messages/retrieve/all')
@@ -24,7 +36,10 @@ class AllMessages extends Component{
 
     componentDidMount(){
         user.setUsername() // on page opened get username
-        setTimeout(funcs.scrollToBottom, 300) // on component is rendered scroll div to bottom
+        setTimeout(()=>{
+            funcs.scrollToBottom()
+            this.setState({onBottom: true})
+        }, 300) // on component is rendered scroll div to bottom
 
         socket.on('recieve-message', msg => { // liten to socket when msg is recieved
             
@@ -35,22 +50,31 @@ class AllMessages extends Component{
     render(){
 
         return (
-            <div id='all-messages'>
-            {this.state.messages.map((msg, i)=>{
-                return (
-                    <div className='message-container'>
-                        <div className='message'>
-                            <div>
-                                <p className='usernames'>{msg.username}</p>
-                            
-                            </div>
-                            <div>
-                                <p className='message-content'>{msg.message}</p>
+            <div id='all-messages-container'>
+                
+                <div id='all-messages' onScroll={this.onScroll}>
+                {this.state.messages.map((msg, i)=>{
+                    return (
+                        <div className='message-container'>
+                            <div className='message'>
+                                <div>
+                                    <p className='usernames'>{msg.username}</p>
+                                
+                                </div>
+                                <div>
+                                    <p className='message-content'>{msg.message}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+                </div>
+                { !this.state.onBottom ? (<div></div>) : (
+                <div id='scroll-bottom'>
+                    
+                    <GrLinkBottom color='rgba(0, 0, 0, 0.80)' size='35px'/>
+                </div>
+                )}
             </div>
         )
     }
