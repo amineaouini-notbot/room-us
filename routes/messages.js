07873c1bdb0c23f1c72043047853e6e87d865644
Geekyume <aminaouini565@gmail.com>
2023-11-22 10:29:19 +0100

    fixed messages retrieval to promise

--- routes/messages.js
+++ routes/messages.js
@@ -3,10 +3,9 @@ const router = express.Router()
 const Messages = require('../models/messageSchema')
 
 router.get('/retrieve/all', (req, res)=>{
-    Messages.find({}, (err, messages)=>{
-        if(err){ res.status(500).send({res: 'error accured!!', error: err})}
-        res.status(200).send({messages})
-    })
+    Messages.find({})
+    .then(messages => res.status(200).send({messages}))
+    .catch(err => res.status(500).send({res: 'error accured!!', error: err}))
 })
 
 module.exports = router;
