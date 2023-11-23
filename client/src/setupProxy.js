df866a30aec0ffa75a56f0a71098900172071698
Geekyume <aminaouini565@gmail.com>
2023-11-23 14:15:55 +0100

    setup production env and connect to client

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     app.use(
       '/api',
       createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: process.env.REACT_APP_API_SERVER,
         changeOrigin: true,
       })
     );
